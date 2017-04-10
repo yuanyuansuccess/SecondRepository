2035a78db296a345f17110f5f676e3f8df205d46
yuan <yuanyuansuccess@163.com>
2017-04-10 22:03:50 +0800

    first commint

--- FirstProject/Form1.cs
+++ FirstProject/Form1.cs
@@ -16,5 +16,10 @@ namespace FirstProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.OutOfMemoryException();
+        }
     }
 }
